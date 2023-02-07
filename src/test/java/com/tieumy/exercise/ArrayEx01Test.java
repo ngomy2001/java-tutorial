f4ddca165dc63ffbeaeafc9fbca98af6da57a215
Võ Quang Hòa <voquanghoa@gmail.com>
2023-02-07 22:21:05 +0700

    Update src/test/java/com/tieumy/exercise/ArrayEx01Test.java

--- src/test/java/com/tieumy/exercise/ArrayEx01Test.java
+++ src/test/java/com/tieumy/exercise/ArrayEx01Test.java
@@ -10,5 +10,8 @@ class ArrayEx01Test {
     void findGreatest() {
         final ArrayEx01 arrayEx01 = new ArrayEx01();
         assertEquals(3, arrayEx01.findGreatest(new int[]{1, 0, 3}));
+        assertEquals(3, arrayEx01.findGreatest(new int[]{1, 0, 3, 2, 1, 0, 1, 2}));
+        assertEquals(4, arrayEx01.findGreatest(new int[]{4, 0, 3, 2, 1, 0, 1, 2}));
+        assertEquals(6, arrayEx01.findGreatest(new int[]{4, 0, 3, 6, 1, 0, 1, 2}));
     }
 }
