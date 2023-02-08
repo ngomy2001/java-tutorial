2485e775acb8c4e507b7472284004358be61b801
Tieu My Ngo <ngomy2001@gmail.com>
2023-02-08 16:00:58 +0700

    Update src/main/java/com/tieumy/exercise/ArrayEx10.java
    Co-authored-by: Võ Quang Hòa <voquanghoa@gmail.com>

--- src/main/java/com/tieumy/exercise/ArrayEx10.java
+++ src/main/java/com/tieumy/exercise/ArrayEx10.java
@@ -5,10 +5,8 @@ public class ArrayEx10 {
     public int[] reverseArray(final int[] arr) {
         int length = arr.length;
         int[] reversedArray = new int[length];
-        int j = length;
-        for (int i = 0; i < length; i++) {
-            reversedArray[j - 1] = arr[i];
-            j--;
+        for (int i = 0, j = length - 1; i < length; i++, j--) {
+            reversedArray[j] = arr[i];
         }
         return reversedArray;
     }
