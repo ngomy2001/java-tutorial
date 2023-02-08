bd892ff731add742226d787d699d35d520c877f1
ngomy2001 <ngomy2001@gmail.com>
2023-02-08 09:58:18 +0700

    fixed

--- src/main/java/com/tieumy/exercise/ArrayEx01.java
+++ src/main/java/com/tieumy/exercise/ArrayEx01.java
@@ -2,15 +2,13 @@ package com.tieumy.exercise;
 
 public class ArrayEx01 {
 
-    public int findGreatest(final int[] arr){
-        int result = arr[0];
-        for(int i = 0; i < arr.length - 1; i++){
-            if(arr[i] < arr[i+1]) {
-                result = arr[i+1];
-            }else{
-                result = arr[i];
+    public int findGreatest(final int[] arr) {
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
             }
         }
-        return result;
+        return max;
     }
 }
