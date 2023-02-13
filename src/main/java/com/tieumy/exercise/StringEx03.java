1c7b556b6d5fc88e70b31fa1c1b11dcd9d11dd17
ngomy2001 <ngomy2001@gmail.com>
2023-02-13 10:41:20 +0700

    changed algorithm

--- src/main/java/com/tieumy/exercise/StringEx03.java
+++ src/main/java/com/tieumy/exercise/StringEx03.java
@@ -3,17 +3,17 @@ package com.tieumy.exercise;
 public class StringEx03 {
 
     public int sumOfIntegersInString(final String input) {
-        String str = input.replaceAll("[^0-9]+", ",");
-        String[] item = str.split(",");
         int sum = 0;
-        for (int i = 0; i < item.length; i++) {
-            if (item[i].equals("")) sum += 0;
-            else {
-                int number = Integer.parseInt(item[i]);
-                sum += number;
+        int x = 0;
+        for (int i = 0; i < input.length(); i++) {
+            final char c = input.charAt(i);
+            if (Character.isDigit(c)) {
+                x = x * 10 + (c - '0');
+            } else {
+                sum += x;
+                x = 0;
             }
-
         }
-        return sum;
+        return sum + x;
     }
 }
