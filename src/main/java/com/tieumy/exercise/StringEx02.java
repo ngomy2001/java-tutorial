e25524677b43a7b6f7638037d406d35a2c5516e7
ngomy2001 <ngomy2001@gmail.com>
2023-02-13 14:00:36 +0700

    changed algorithm

--- src/main/java/com/tieumy/exercise/StringEx02.java
+++ src/main/java/com/tieumy/exercise/StringEx02.java
@@ -2,18 +2,16 @@ package com.tieumy.exercise;
 
 public class StringEx02 {
 
-    public String reverseString(final String input) {
-        final StringBuilder str = new StringBuilder();
-        for (int i = input.length() - 1; i >= 0; i--) {
-            str.append(input.charAt(i));
-        }
-        return str.toString();
-    }
-
     public boolean isSymmertrical(final String input) {
-
-        String reversedString = reverseString(input);
-        if (reversedString.equals(input)) return true;
-        return false;
+        int first = 0;
+        int last = input.length() - 1;
+        while (first <= last) {
+            if (input.charAt(first) != input.charAt(last)) {
+                return false;
+            }
+            first++;
+            last--;
+        }
+        return true;
     }
 }
--- src/test/java/com/tieumy/exercise/StringEx02Test.java
+++ src/test/java/com/tieumy/exercise/StringEx02Test.java
@@ -2,25 +2,17 @@ package com.tieumy.exercise;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StringEx02Test {
 
     @Test
-    void reverseString() {
-        final StringEx02 stringEx02 = new StringEx02();
-        String input = "Independent";
-        assertEquals("tnednepednI", stringEx02.reverseString(input));
-    }
-
-    @Test
     void isSymmertrical() {
         final StringEx02 stringEx02 = new StringEx02();
-        String validInput = "abcdcba";
-        String invalidInput = "abcde";
-        boolean valueTrue = true;
-        boolean valueFalse = false;
-        assertEquals(valueTrue, stringEx02.IsSymmertrical(validInput));
-        assertEquals(valueFalse, stringEx02.IsSymmertrical(invalidInput));
+        assertTrue(stringEx02.isSymmertrical("abcdcba"));
+        assertTrue(stringEx02.isSymmertrical("abcddcba"));
+        assertTrue(stringEx02.isSymmertrical(" "));
+        assertFalse(stringEx02.isSymmertrical("abcd"));
     }
 }
