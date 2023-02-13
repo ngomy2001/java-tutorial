d160f65bfe6661ab840f867a0eb5ab808bc83ac4
ngomy2001 <ngomy2001@gmail.com>
2023-02-13 09:57:14 +0700

    using final StringBuilder

--- src/main/java/com/tieumy/exercise/StringEx01.java
+++ src/main/java/com/tieumy/exercise/StringEx01.java
@@ -2,7 +2,7 @@ package com.tieumy.exercise;
 
 public class StringEx01 {
     public String reverseString(final String input) {
-        StringBuilder str = new StringBuilder();
+        final StringBuilder str = new StringBuilder();
         for (int i = input.length() - 1; i >= 0; i--) {
             str.append(input.charAt(i));
         }
