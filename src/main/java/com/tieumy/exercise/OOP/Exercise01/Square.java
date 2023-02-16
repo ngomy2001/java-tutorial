84c588b5a22f6149a712203d7cf9382c31ccf23a
ngomy2001 <ngomy2001@gmail.com>
2023-02-16 15:32:00 +0700

    updated contains in Square

--- src/main/java/com/tieumy/exercise/OOP/Exercise01/Square.java
+++ src/main/java/com/tieumy/exercise/OOP/Exercise01/Square.java
@@ -1,46 +1,14 @@
 package com.tieumy.exercise.OOP.Exercise01;
 
 public class Square extends Rectangle {
-    private Point topRight;
-    private double side;
+    private final double width;
 
-    public double getSide() {
-        return side;
+    public Square(final Point topRight, final double width) {
+        super(topRight, width, width);
+        this.width = width;
     }
-
-    public void setSide(double side) {
-        this.side = side;
-    }
-
-    public Square() {
-
-    }
-
-    @Override
-    public double getArea() {
-
-        return side * side;
-    }
-
-    @Override
-    public double getPerimeter() {
-
-        return side * 4;
-    }
-
-    @Override
+    
     public boolean contains(Point point) {
-        Point bottomLeft = new Point();
-        bottomLeft.setX(topRight.getX() - side);
-        bottomLeft.setY(topRight.getY() - side);
-
-        if (!(point.getX() > bottomLeft.getX()) && point.getX() < topRight.getX()) {
-            return false;
-        }
-
-        if (!(point.getY() > bottomLeft.getY() && point.getY() < topRight.getY())) {
-            return false;
-        }
-        return true;
+        return super.contains(point);
     }
 }
