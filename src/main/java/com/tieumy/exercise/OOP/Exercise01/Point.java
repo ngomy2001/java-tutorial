4eb927ffce7ecf6ea2de21f527994f76d0d393b4
Tieu My Ngo <ngomy2001@gmail.com>
2023-02-17 11:12:06 +0700

    Update src/main/java/com/tieumy/exercise/OOP/Exercise01/Point.java
    Co-authored-by: Võ Quang Hòa <voquanghoa@gmail.com>

--- src/main/java/com/tieumy/exercise/OOP/Exercise01/Point.java
+++ src/main/java/com/tieumy/exercise/OOP/Exercise01/Point.java
@@ -1,8 +1,8 @@
 package com.tieumy.exercise.OOP.Exercise01;
 
 public class Point {
-    private double x;
-    private double y;
+    private final double x;
+    private final double y;
 
     public Point() {
 
