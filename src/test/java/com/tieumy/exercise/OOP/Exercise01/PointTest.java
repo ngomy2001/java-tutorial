e99db19cb7c73908ede44e2fa00db97268912b4f
ngomy2001 <ngomy2001@gmail.com>
2023-02-17 10:44:26 +0700

    unit tests for Point

--- src/test/java/com/tieumy/exercise/OOP/Exercise01/PointTest.java
+++ src/test/java/com/tieumy/exercise/OOP/Exercise01/PointTest.java
@@ -2,10 +2,20 @@ package com.tieumy.exercise.OOP.Exercise01;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 class PointTest {
+    final Point a = new Point(2, 7);
+    final Point b = new Point(3, 4);
+
+    final Point c = new Point(-3, 5);
 
     @Test
     void getX() {
+        assertEquals(2, a.getX());
+        assertEquals(3, b.getX());
+        assertEquals(-3, c.getX());
     }
 
     @Test
@@ -14,6 +24,9 @@ class PointTest {
 
     @Test
     void getY() {
+        assertEquals(7, a.getY());
+        assertEquals(4, b.getY());
+        assertEquals(5, c.getY());
     }
 
     @Test
