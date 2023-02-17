7308c688cc236aaa426799816fa3e8e7b283b4b6
Tieu My Ngo <ngomy2001@gmail.com>
2023-02-17 11:12:20 +0700

    Update src/main/java/com/tieumy/exercise/OOP/Exercise01/Point.java
    Co-authored-by: Võ Quang Hòa <voquanghoa@gmail.com>

--- src/main/java/com/tieumy/exercise/OOP/Exercise01/Point.java
+++ src/main/java/com/tieumy/exercise/OOP/Exercise01/Point.java
@@ -5,7 +5,7 @@ public class Point {
     private final double y;
 
 
-    public Point(double x, double y) {
+    public Point(final double x, final double y) {
         this.x = x;
         this.y = y;
     }
