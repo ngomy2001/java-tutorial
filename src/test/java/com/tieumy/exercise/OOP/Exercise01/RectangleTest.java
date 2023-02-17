1de8a30289ca8b76a9261eea07261c90e1f74404
ngomy2001 <ngomy2001@gmail.com>
2023-02-17 10:13:29 +0700

    unit tests for Rectangle

--- src/test/java/com/tieumy/exercise/OOP/Exercise01/RectangleTest.java
+++ src/test/java/com/tieumy/exercise/OOP/Exercise01/RectangleTest.java
@@ -2,25 +2,34 @@ package com.tieumy.exercise.OOP.Exercise01;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class RectangleTest {
+    final Rectangle rectangle = new Rectangle(new Point(2, 3), 5, 4);
 
     @Test
     void getLength() {
+        assertEquals(5, rectangle.getLength());
     }
 
     @Test
     void getWidth() {
+        assertEquals(4, rectangle.getWidth());
     }
 
     @Test
     void getArea() {
+        assertEquals(20, rectangle.getArea());
     }
 
     @Test
     void getPerimeter() {
+        assertEquals(18, rectangle.getPerimeter());
     }
 
     @Test
     void contains() {
+        assertTrue(rectangle.contains(new Point(0, 0)));
+        assertFalse(rectangle.contains(new Point(1, 6)));
     }
 }
