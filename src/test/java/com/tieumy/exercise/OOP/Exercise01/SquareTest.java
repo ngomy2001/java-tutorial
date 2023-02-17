5457a372077bb0c4e31158c404c453d3850ea9c6
ngomy2001 <ngomy2001@gmail.com>
2023-02-17 10:40:28 +0700

    unit tests for Square

--- src/test/java/com/tieumy/exercise/OOP/Exercise01/SquareTest.java
+++ src/test/java/com/tieumy/exercise/OOP/Exercise01/SquareTest.java
@@ -2,9 +2,13 @@ package com.tieumy.exercise.OOP.Exercise01;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class SquareTest {
+    final Square square = new Square(new Point(4, 5), 3);
 
     @Test
     void getSide() {
+        assertEquals(3, square.getSide());
     }
 }
