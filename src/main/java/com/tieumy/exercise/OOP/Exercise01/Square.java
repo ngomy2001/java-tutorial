246c17784c01acc4a427cd56de0875cef3b5202a
Tieu My Ngo <ngomy2001@gmail.com>
2023-02-17 11:16:16 +0700

    Update src/main/java/com/tieumy/exercise/OOP/Exercise01/Square.java
    Co-authored-by: Võ Quang Hòa <voquanghoa@gmail.com>

--- src/main/java/com/tieumy/exercise/OOP/Exercise01/Square.java
+++ src/main/java/com/tieumy/exercise/OOP/Exercise01/Square.java
@@ -3,7 +3,7 @@ package com.tieumy.exercise.OOP.Exercise01;
 public class Square extends Rectangle {
     private final double side;
 
-    public Square(final Point topRight, final double side) {
+    public Square(final Point topRight, final double width) {
         super(topRight, side, side);
         this.side = side;
     }
