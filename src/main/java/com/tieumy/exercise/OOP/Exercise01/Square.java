34890ff9cca39ef4effd4ab029c706095b081a1d
ngomy2001 <ngomy2001@gmail.com>
2023-02-17 16:34:33 +0700

    removed-side-field

--- src/main/java/com/tieumy/exercise/OOP/Exercise01/Square.java
+++ src/main/java/com/tieumy/exercise/OOP/Exercise01/Square.java
@@ -1,14 +1,8 @@
 package com.tieumy.exercise.OOP.Exercise01;
 
 public class Square extends Rectangle {
-    private final double side;
-
-    public Square(final Point topRight, final double width) {
+    public Square(final Point topRight, final double side) {
         super(topRight, side, side);
-        this.side = side;
     }
-
-    public double getSide() {
-        return side;
-    }
+    
 }
