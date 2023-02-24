579c37fcef3ea592dfc3e1a5260318077757725d
ngomy2001 <ngomy2001@gmail.com>
2023-02-24 13:45:38 +0700

    example-of-JSON

--- src/main/java/com/tieumy/exercise/JSONExample/JacksonObjectMapperExample.java
+++ src/main/java/com/tieumy/exercise/JSONExample/JacksonObjectMapperExample.java
@@ -1,15 +1,62 @@
 package com.tieumy.exercise.JSONExample;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 import java.io.IOException;
+import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class JacksonObjectMapperExample {
     public static void main(String[] args) throws IOException {
 
         //read json file data to String
-        byte[] jsonData = Files.readAllBytes(Paths.get("employee.txt"));
+        byte[] jsonData = Files.readAllBytes(Paths.get("/Users/ngomy2001/Desktop/JAVA/JavaExercise/src/main/java/com/tieumy/exercise/JSONExample/employee.txt"));
 
         //create ObjectMapper instance
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        //convert json string to object
+        Employee empl = objectMapper.readValue(jsonData, Employee.class);
+
+        System.out.println("Employee Object\n" + empl);
+
+        //Convert object to json string
+        objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+
+        //writing to console, can write to any output stream such as file
+        StringWriter stringEmp = new StringWriter();
+        objectMapper.writeValue(stringEmp, empl);
+        System.out.println("Employee JSON is\n" + stringEmp);
+    }
+
+    public static Employee createEmployee() {
+        Employee emp = new Employee();
+        emp.setId(100);
+        emp.setName("David");
+        emp.setPermanent(false);
+        emp.setPhoneNumbers(new long[]{123456, 987654});
+        emp.setRole("Manager");
+
+        Address add = new Address();
+        add.setCity("Bangalore");
+        add.setStreet("BTM 1st stage");
+        add.setZipcode(560100);
+
+        List<String> cities = new ArrayList<String>();
+        cities.add("Los Angeles");
+        cities.add("New York");
+        emp.setCities(cities);
+
+        Map<String, String> props = new HashMap<String, String>();
+        props.put("salary", "1000 Rs");
+        props.put("age", "28 years");
+        emp.setProperties(props);
+        return emp;
     }
 }
